bdd17c78b5433f6f6bd771169aec4e2a9dc481c6
Carolina Ruiz <ing.acrd@gmail.com>
2024-07-19 09:36:56 -0400

    flatList and SectionList

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './components/WelcomeScreen';
 import MenuItems from './components/MenuItems';
+import MenuItemsFlatList from './components/MenuItemsFlatList';
 
 export default function App() {
   return (
